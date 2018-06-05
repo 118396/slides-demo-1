bee9bbe36f72d09201e57cb8697f04c329f7e050
weihu <1183969348@qq.com>
2018-06-05 22:59:00 +0800

    slides-demo-2

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-let n = 1
+let n
+初始化()
 setInterval(()=>{
- 
-$(`.images>img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-.one('transitionend',(e)=>{
-    $(e.currentTarget).removeClass('leave').addClass('enter')
-}) 
-$(`.images>img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
-n += 1
+  makeLeave(getImage(n))
+    .one('transitionend', (e)=>{
+      makeEnter($(e.currentTarget))//let 当前元素 = e.currentTarget
+                                    //makeEnter($(当前元素))
+    })
+  makeCurrent(getImage(n+1))
+  n += 1
 },3000)
+//声明一个n，初始化它，每三秒钟让第n个emg走（leave），leave结束以后，便进入enter区。
+//让n+1个变成当前元素，现在n变成2，再过三秒让第2个元素走，进去enter区，同时n+1让第3个元
+//素变成当前元素，现在n变成3，再过三秒让第3个元素走，进去enter区，让第一个元素进来。
+
+
+function getImage(n){
+    return $(`.images>img:nth-child(${x(n)})`)
+}
 
 function x(n){
     if(n>3){
@@ -20,4 +26,21 @@ function x(n){
         }
     }
     return n
+}
+
+function 初始化(){
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    return  $node.removeClass('enter current').addClass('leave')
+    
+}
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
 }
